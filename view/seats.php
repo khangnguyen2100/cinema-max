6a7e51c246c73cf66d53409784bf0a3d9522ebf5
khangnguyen2100 <khangnguyen23112100@gmail.com>
2022-11-20 21:27:29 +0700

    initl seats page

---
+++ view/seats.php
@@ -0,0 +1,272 @@
+<div class="seat-container">
+
+    <div class="linealphabet">
+        <h2>CHỌN GHẾ</h2>
+        <a href="">
+            <div class="alphabet">
+                <P>P</P>
+            </div>
+        </a>
+
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+        <br>
+        <a href="">
+            <div class="alphabet">
+                <P>O</P>
+
+            </div>
+        </a>
+
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+        <a href="">
+            <div class="alphabet">
+                <P>N</P>
+            </div>
+        </a>
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+        <a href="">
+            <div class="alphabet">
+                <P>M</P>
+            </div>
+        </a>
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+        <a href="">
+            <div class="alphabet">
+                <P>L</P>
+            </div>
+
+        </a>
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+        <a href="">
+            <div class="alphabet">
+                <P>K</P>
+            </div>
+        </a>
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+        <a href="">
+            <div class="alphabet">
+                <P>J</P>
+            </div>
+        </a>
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+        <a href="">
+            <div class="alphabet">
+                <P>I</P>
+            </div>
+        </a>
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+        <a href="">
+            <div class="alphabet">
+                <P>H</P>
+            </div>
+        </a>
+        <div class="linenumber">
+            <button type="submit" click="click">1</button>
+            <button type="submit" click="click">2</button>
+            <button type="submit" click="click">3</button>
+            <button type="submit" click="click">4</button>
+            <button type="submit" click="click">5</button>
+            <button type="submit" click="click">6</button>
+            <button type="submit" click="click">7</button>
+            <button type="submit" click="click">8</button>
+            <button type="submit" click="click">9</button>
+            <button type="submit" click="click">10</button>
+            <button type="submit" click="click">11</button>
+            <button type="submit" click="click">12</button>
+            <button type="submit" click="click">13</button>
+            <button type="submit" click="click">14</button>
+            <button type="submit" click="click">15</button>
+            <button type="submit" click="click">16</button>
+            <button type="submit" click="click">17</button>
+            <button type="submit" click="click">18</button>
+            <button type="submit" click="click">19</button>
+
+        </div>
+
+    </div>
+    <br><br>
+    <div class="manhinh">
+        <p>MÀN HÌNH</p>
+    </div>
+    <hr>
+    <div class="note">
+        <p>Ghế đang chọn</p>
+    </div>
+    <div class="note1">
+        <p>Ghế đã bán</p>
+    </div>
+    <div class="note2">
+        <p>Có thể chọn</p>
+    </div>
+    <div class="note3">
+        <p>Không thể chọn</p>
+    </div>
+</div>
